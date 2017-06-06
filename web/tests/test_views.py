2f303b42f6394b5f27a1edb800ad6efc7df229c0
Isaac Serafino <isaacserafino@users.noreply.github.com>
2017-06-06 13:14:01 -0500

    Unit Tests - Implement unit tests for AdministrationService. -IMS

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+import dropbox
+from unittest import mock
 
+from py_snap_screen import settings
+from web import views
 from web.models import Activity
 from web.models import SupervisorId
 from web.models import ViewerConnection
@@ -7,6 +11,7 @@ from web.models import ViewerConnectionService
 from web.models import ViewerService
 from web.models import SupervisorIdService
 from web.models import PersistenceService
+from web.tests import stubs
 from web.views import AdministrationService
 from web.views import AdministrationView
 from web.views import MonitoringService
@@ -16,22 +21,62 @@ from web.views import MonitoringView
 
 # View Tests
 class AdministrationViewTest(TestCase):
-    pass
+    def setUp(self):
+        pass
 
 
 class MonitoringViewTest(TestCase):
-    pass
+    def setUp(self):
+        pass
 
 
 class ViewerConnectionCallbackViewTest(TestCase):
-    pass
+    def setUp(self):
+        pass
 
 
 # Business Service Tests
 class AdministrationServiceTest(TestCase):
-    def setUp(self):
-        pass
+    @mock.patch("web.views.persistence_service", autospec=True)
+    @mock.patch("web.views.supervisor_id_service", autospec=True)
+    @mock.patch("web.views.viewer_connection_service", autospec=True)
+    def setUp(self, mock_viewer_connection_service, mock_supervisor_id_service, mock_persistence_service):
+        self.mock_viewer_connection_service = mock_viewer_connection_service
+        self.mock_supervisor_id_service = mock_supervisor_id_service
+        self.mock_persistence_service = mock_persistence_service
+        self.candidate = AdministrationService(views.persistence_service, views.supervisor_id_service, views.viewer_connection_service)
 
+        self.flow = mock.create_autospec(dropbox.DropboxOAuth2Flow)
+        self.mock_viewer_connection_service.create_flow_object.return_value = self.flow
+
+    def test_start_creating_supervisor_id(self):
+        session = {}
+        
+        self.mock_viewer_connection_service.start_creating_connection.return_value = stubs.AUTHORIZATION_URL
+
+        actual_authorization_url = self.candidate.start_creating_supervisor_id(session)
+        
+        self._assert_created_flow(session)
+        self.mock_viewer_connection_service.start_creating_connection.assert_called_once_with(self.flow)
+        
+        self.assertEqual(stubs.AUTHORIZATION_URL, actual_authorization_url)
+
+    def test_finish_creating_supervisor_id(self):
+        callback_parameters = {"state": "xyz"}
+        session = {"dropbox-auth-csrf-token": "xyz"}
+
+        self.mock_viewer_connection_service.finish_creating_connection.return_value = stubs.CONNECTION
+        self.mock_supervisor_id_service.generate.return_value = stubs.SUPERVISOR_ID
+
+        actual_supervisor_id = self.candidate.finish_creating_supervisor_id(callback_parameters, session)
+
+        self._assert_created_flow(session)
+        self.mock_viewer_connection_service.finish_creating_connection.assert_called_once_with(callback_parameters, self.flow)
+        self.mock_persistence_service.save_viewer_connection.assert_called_once_with(stubs.CONNECTION, stubs.SUPERVISOR_ID)
+        self.assertEqual(stubs.SUPERVISOR_ID, actual_supervisor_id)
+
+    def _assert_created_flow(self, session):
+        self.mock_viewer_connection_service.create_flow_object.assert_called_once_with(settings.DROPBOX_API_KEY, settings.DROPBOX_API_SECRET, settings.DROPBOX_CALLBACK_URL, session, "dropbox-auth-csrf-token")
 
 class MonitoringServiceTest(TestCase):
     def setUp(self):
