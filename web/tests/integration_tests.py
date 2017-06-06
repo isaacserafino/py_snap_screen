dcb2cdbf89736856a5f3210bdfd5ed84e02a2d1b
Isaac Serafino <isaacserafino@users.noreply.github.com>
2017-06-06 13:51:10 -0500

    Tests - Extract test data into separate test stubs module. -IMS

--- web/tests/integration_tests.py
+++ web/tests/integration_tests.py
@@ -9,38 +9,32 @@ from web.models import SupervisorIdService
 from web.models import ViewerConnection
 from web.models import ViewerConnectionService
 from web.models import ViewerService
+from web.tests import stubs
 from web.views import AdministrationService
 
 # TODO: (IMS) Change from service tests to view tests
 
 class PersistenceServiceTest(TestCase):
-    STUB_ACTIVE = True
-    STUB_AUTHORIZATION_TOKEN = "stub_authorization_token"
-    STUB_SUPERVISOR_ID_VALUE = "3oe2UAP"
-
-    connection = ViewerConnection(STUB_ACTIVE, STUB_AUTHORIZATION_TOKEN)
-    supervisor_id = SupervisorId(STUB_SUPERVISOR_ID_VALUE)
-
     def setUp(self):
         self.candidate = core_service_factory.createPersistenceService()
 
     def test_save_viewer_connection(self):
-        self.candidate.save_viewer_connection(self.connection, self.supervisor_id)
+        self.candidate.save_viewer_connection(stubs.CONNECTION, stubs.SUPERVISOR_ID)
         
-        supervisor = core_service_factory.core_persistence_service.objects.get(supervisor_id=self.STUB_SUPERVISOR_ID_VALUE)
+        supervisor = core_service_factory.core_persistence_service.objects.get(supervisor_id=stubs.SUPERVISOR_ID_VALUE)
 
-        self.assertEqual(self.STUB_ACTIVE, supervisor.active)
-        self.assertEqual(self.STUB_AUTHORIZATION_TOKEN, supervisor.viewer_authentication_key)
-        self.assertEqual(self.STUB_SUPERVISOR_ID_VALUE, supervisor.supervisor_id)
+        self.assertEqual(stubs.ACTIVE, supervisor.active)
+        self.assertEqual(stubs.AUTHORIZATION_TOKEN, supervisor.viewer_authentication_key)
+        self.assertEqual(stubs.SUPERVISOR_ID_VALUE, supervisor.supervisor_id)
 
     def test_retrieve_viewer_connection(self):
-        supervisor = core_service_factory.core_persistence_service(active=self.STUB_ACTIVE, supervisor_id=self.STUB_SUPERVISOR_ID_VALUE, viewer_authentication_key=self.STUB_AUTHORIZATION_TOKEN)
+        supervisor = core_service_factory.core_persistence_service(active=stubs.ACTIVE, supervisor_id=stubs.SUPERVISOR_ID_VALUE, viewer_authentication_key=stubs.AUTHORIZATION_TOKEN)
         supervisor.save()
 
-        actual_connection = self.candidate.retrieve_viewer_connection(self.supervisor_id)
+        actual_connection = self.candidate.retrieve_viewer_connection(stubs.SUPERVISOR_ID)
 
-        self.assertEqual(self.connection.active, actual_connection.active)
-        self.assertEqual(self.connection.authorization_token, actual_connection.authorization_token)
+        self.assertEqual(stubs.ACTIVE, actual_connection.active)
+        self.assertEqual(stubs.AUTHORIZATION_TOKEN, actual_connection.authorization_token)
 
 
 class SupervisorIdServiceTest(TestCase):
@@ -71,26 +65,22 @@ class AdministrationServiceTest(TestCase):
 
 class ViewerServiceTest(TestCase):
     AUTHORIZATION_TOKEN = settings.TEST_AUTHORIZATION_TOKEN
-    STUB_CONTENTS = "stub_contents".encode()
-    STUB_FILENAME = "/stub_filename.txt"
-
-    activity = Activity(STUB_FILENAME, STUB_CONTENTS)
     connection = ViewerConnection(True, AUTHORIZATION_TOKEN)
 
     def setUp(self):
         self.candidate = core_service_factory.createViewerService()
 
     def test_send_activity(self):
-        self.candidate.send_activity(self.activity, self.connection)
+        self.candidate.send_activity(stubs.ACTIVITY, self.connection)
 
         api = core_service_factory.core_viewer_service(self.AUTHORIZATION_TOKEN)
-        metadata, resource = api.files_download(self.STUB_FILENAME)
+        metadata, resource = api.files_download(stubs.FILENAME)
 
-        self.assertEqual(self.STUB_CONTENTS, resource.content)
+        self.assertEqual(stubs.CONTENTS, resource.content)
 
     def tearDown(self):
         api = core_service_factory.core_viewer_service(self.AUTHORIZATION_TOKEN)
-        api.files_delete(self.STUB_FILENAME)
+        api.files_delete(stubs.FILENAME)
 
 
 core_service_factory = core.CoreServiceFactory()
